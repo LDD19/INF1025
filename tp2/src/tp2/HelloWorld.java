5ac221926c118be69a88c10660143d23c77cf791
Frédéric Achard-Groleau <achard.groleau.frederic@gmail.com>
2020-11-26 16:36:36 -0500

    changement hello world

--- tp2/src/tp2/HelloWorld.java
+++ tp2/src/tp2/HelloWorld.java
@@ -2,6 +2,9 @@ package tp2;
 
 public class HelloWorld {
 	    public static void main(String[] args){
-	        System.out.println("Hello World!");
+	        for(int i = 0; i < 5; ++i) {
+	        	System.out.println("Hello World!");
+	        }
+	        
 	    }
 }
